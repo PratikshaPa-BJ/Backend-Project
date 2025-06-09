5303d4d20c3b547f3fe0f4de0381a6ddb4d6646e
Pratiksha-pari <parihar.pratiksh59@gmail.com>
2025-06-09 20:17:44 +0530

    First API

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,4 +4,8 @@ const router = express.Router();
 router.get('/test-me', function(req,res){
     res.send("My First Node JS Application");
 })
+
+router.get('/test-you', function(req,res){
+    res.send("My Second API ")
+})
  module.exports = router;
