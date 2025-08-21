1f985f14c77d8209ad8dfb0358161b74d3508b44
Pratiksha-pari <parihar.pratiksh59@gmail.com>
2025-08-22 02:48:35 +0530

    Done

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,28 +2,34 @@ const jwt = require("jsonwebtoken");
 const mongoose = require("mongoose");
 
 const tokenValidation = function (req, res, next) {
-  let token = req.headers["x-auth-token"];
-  if (!token) {
-    return res.send({ status: false, msg: "Token must be present.." });
-  }
-  let decodedToken = jwt.verify(token, "seversidesecretkey");
-  // console.log(decodedToken);
+  try {
+    let token = req.headers["x-auth-token"];
+    if (!token) {
+      return res.send({ status: false, msg: "Token must be present.." });
+    }
+    let decodedToken = jwt.verify(token, "seversidesecretkey");
+    // console.log(decodedToken);
 
-  if (!decodedToken) {
-    return res.send({ status: false, msg: "Token is invalid" });
-  }
-  req.userIdFromDecodedToken = decodedToken.userId;
+    if (!decodedToken) {
+      return res.status(401).send({ status: false, msg: "Token is invalid" });
+    }
+    req.userIdFromDecodedToken = decodedToken.userId;
 
-  next();
+    next();
+  } catch (error) {
+    res.status(500).send({ status: false, msg: error.message });
+  }
 };
 
 const authorizationf = function (req, res, next) {
   let userIdReq = req.params.userId;
   if (!mongoose.isValidObjectId(userIdReq)) {
-    return res.send({ status: false, msg: "Please provide valid user id.." });
+    return res
+      .status(400)
+      .send({ status: false, msg: "Please provide valid user id.." });
   }
   if (req.userIdFromDecodedToken !== userIdReq) {
-    return res.send({
+    return res.status(403).send({
       status: false,
       msg: "You don't have authorisation to do this..",
     });
--- src/models/userModel.js
+++ src/models/userModel.js
@@ -27,4 +27,4 @@ const userSchema = new mongoose.Schema(
   },
   { timestamps: true }
 );
-module.exports = mongoose.model("userD", userSchema);
+module.exports = mongoose.model("userE", userSchema);
